83f8880a06129195426deab784d667dd82354464
HninNandarAung <hninnandaraung6299@gamil.com>
2020-09-12 09:15:41 +0630

    modify

--- Myproj/src/hello.java
+++ Myproj/src/hello.java
@@ -2,6 +2,8 @@
 public class hello {
 	static void myMethod() {
 		System.out.println("hello World");
+		System.out.println("hello World I'm a human");
+		
 	}
 public  static void main(String[]args) {
 	myMethod();
